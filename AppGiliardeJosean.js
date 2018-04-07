a27af30247f90e064181ae2240a4daebf9f4f2e6
JOSEAM <joseam-boy@hotmail.com>
2018-04-07 09:49:01 -0300

    Geolocalização IFRN CN

--- AppGiliardeJosean.js
+++ AppGiliardeJosean.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {
   StyleSheet,
@@ -7,8 +6,8 @@ import {
   Dimensions,
   TouchableOpacity,
 } from 'react-native';
-
 import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps';
+import MyLocationMapMarker from './MyLocationMapMarker';
 
 const { width, height } = Dimensions.get('window');
 
@@ -17,10 +16,8 @@ const LATITUDE = -6.253380;
 const LONGITUDE = -36.53454;
 const LATITUDE_DELTA = 0.0050;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-let id = 0;
 
-
-class PolygonCreator extends React.Component {
+class BugMarkerWontUpdate extends React.Component {
   constructor(props) {
     super(props);
 
@@ -31,32 +28,18 @@ class PolygonCreator extends React.Component {
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       },
-      polygons: [],
-      editing: null,
-      creatingHole: false,
+      coordinate: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+      },
+      amount: 0,
+      enableHack: false,
     };
   }
 
-  finish() {
-    const { polygons, editing } = this.state;
-    this.setState({
-      polygons: [...polygons, editing],
-      editing: null,
-      creatingHole: false,
-    });
-  }
 
-  
+
   render() {
-    const mapOptions = {
-      scrollEnabled: true,
-    };
-
-    if (this.state.editing) {
-      mapOptions.scrollEnabled = false;
-      mapOptions.onPanDrag = e => this.onPress(e);
-    }
-
     return (
       <View style={styles.container}>
         <MapView
@@ -64,56 +47,21 @@ class PolygonCreator extends React.Component {
           style={styles.map}
           mapType={MAP_TYPES.HYBRID}
           initialRegion={this.state.region}
-          onPress={e => this.onPress(e)}
-          {...mapOptions}
+          
         >
-          {this.state.polygons.map(polygon => (
-            <Polygon
-              key={polygon.id}
-              coordinates={polygon.coordinates}
-              holes={polygon.holes}
-              strokeColor="#F00"
-              fillColor="rgba(255,0,0,0.5)"
-              strokeWidth={1}
-            />
-          ))}
-          {this.state.editing && (
-            <Polygon
-              key={this.state.editing.id}
-              coordinates={this.state.editing.coordinates}
-              holes={this.state.editing.holes}
-              strokeColor="#000"
-              fillColor="rgba(255,0,0,0.5)"
-              strokeWidth={1}
-            />
-          )}
+          <MyLocationMapMarker
+            coordinate={this.state.coordinate}
+            heading={this.state.amount}
+            enableHack={this.state.enableHack}
+          />
         </MapView>
-        <View style={styles.buttonContainer}>
-          {this.state.editing && (
-            <TouchableOpacity
-              onPress={() => this.createHole()}
-              style={[styles.bubble, styles.button]}
-            >
-              <Text>{this.state.creatingHole ? 'Finish Hole' : 'Create Hole'}</Text>
-            </TouchableOpacity>
-          )}
-          {this.state.editing && (
-            <TouchableOpacity
-              onPress={() => this.finish()}
-              style={[styles.bubble, styles.button]}
-            >
-              <Text>Finish</Text>
-            </TouchableOpacity>
-          )}
-        </View>
+      
+        
       </View>
     );
   }
 }
 
-PolygonCreator.propTypes = {
-  provider: ProviderPropType,
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -130,21 +78,9 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 20,
   },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
-  },
-});
 
-export default PolygonCreator;
+  
+  
+  });
+
+export default BugMarkerWontUpdate;
