b7d8e82da2ccd041cb0eac532f0bc67c63ce6c1e
kairiroberto <kairiroberto@gmail.com>
2018-04-07 02:33:20 -0300

    Geolocation - Roberto e Gabriel
    Não estou conseguindo desenhar o mapa no meu projeto, mas estou tentando criar uma funcionalidade que com base na latitude e longitude do IFRN, o sistema verifica se o usuário está próximo ou não do IF. Para fazer isso coloquei um botão

--- App.js
+++ App.js
@@ -1,23 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.cont}>
-        <Text>Open up App to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+import React, { Component } from 'react';
+import { Text, View, Alert, TouchableOpacity } from 'react-native';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitudeif: 37.421998333333335,
+            longitudeif: -122.08400000000002,
+            latitude: null,
+            longitude: null,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState(
+                    {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        error: null,
+                    }
+                );
+            },
+            (error) => this.setState(
+                    {
+                        error: error.message
+                    }),
+                    {
+                        enableHighAccuracy: true, timeout: 20000, maximumAge: 1000
+                    },
+        );
+    }
+    
+    
+    
+    onPress = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState(
+                    {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        error: null,
+                    }
+                );
+            },
+            (error) => this.setState(
+                    {
+                        error: error.message
+                    }),
+                    {
+                        enableHighAccuracy: true, timeout: 20000, maximumAge: 1000
+                    },
+        );
+        if ((this.state.latitude > this.state.latitudeif - 10) && (this.state.latitude < this.state.latitudeif + 10)) {
+            if ((this.state.longitude > this.state.longitudeif - 10) && (this.state.longitude < this.state.longitudeif + 10)) {
+                Alert.alert('Você está no IFRN de Currais Novos');
+            } else {
+                Alert.alert('Você não está no IFRN de Currais Novos');
+            }
+        } else {
+            Alert.alert('Você não está no IFRN de Currais Novos');
+        }
+    }
+
+    render() {
+        return (
+                <View>
+                    <Text>
+                    Welcome to React Native!
+                    </Text>
+                    <Text>
+                        Latitude: {this.state.latitude}
+                    </Text>
+                    <Text>
+                        Longitude: {this.state.longitude}
+                    </Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                    <TouchableOpacity
+                        onPress={this.onPress}
+                    >
+                    <Text> Touch Here </Text>
+                    </TouchableOpacity>
+                </View>
+                );
+
+    }
+
 }
 
-const styles = StyleSheet.create({
-  cont: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
