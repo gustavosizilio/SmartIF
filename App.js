3d608bb382a4d3b6b2432d8657f5d83008376e7c
Gabriel Adriano <gabrielpapel8@gmail.com>
2018-05-06 10:37:00 -0300

    Atualização master local

--- App.js
+++ App.js
@@ -1,219 +1,65 @@
-import React from 'react';
-import { Component, TouchableHighlight,Navigator, StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, AsyncStorage, Alert} from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
+import React, { Component } from 'react';
+import { Button, Text } from 'react-native';
+import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
+import Mapa from './mapa/AppMapa';
+import Home from './login/AppHome';
+import Login from './login/AppLogin';
 
-
-const ACCESS_TOKEN = 'access_token';
-
-export default class App extends React.Component {
-
-render() {
-    return (
-      <View style={styles.container}>
-            <ImageBackground source={require('./app/img/ifrncn.png')} style={styles.backgroundImage}>
-                  <View style={styles.content}>
-                       <Text style={styles.logo}> ACESSAR </Text>
-                             <View style={styles.inputContainer}>
-                                  <TextInput underlineColorAndroid='transparent' style={styles.input} 
-                                  onChangeText={(username) => this.setState({username})}
-                                  value={this.state.username}>
-                                  </TextInput>
-
-                                  <TextInput secureTextEntry={true} underlineColorAndroid='transparent'  
-                                  placeholder='password' 
-                                  onChangeText={(password) => this.setState({password})}
-                                  value={this.state.password}
-                                  style={styles.input}>
-                                  </TextInput>
-                             </View>
-                              <TouchableOpacity onPress={this.login.bind(this)} style={styles.buttonContainer}>
-                                  <Text style={styles.buttonText}> LOGIN </Text>
-                              </TouchableOpacity>
-                  </View>
-            </ImageBackground>
-      </View>
-    );
-  }
-
-   constructor(props){
-          super(props);
-          this.state = {username: "", password: "", error: ""};
-
-        }
-
-  componentDidMount() {
-    this.getToken();
-  }
-
-  navigate(routeName){
-    this.props.navigator.push({
-      name: routeName
-    })
-   }
-
-    verifyToken(token) {
-
-   var token = token
-
-    try {
-     response = fetch('https://suap.ifrn.edu.br/api/v2/autenticacao/token/verify/'+token);
-      
-     res = response.text();
-
-      if (response.token) {
-
-      //Token confirmado significa que o usuário está logado, então o redirecionamos para home
-      
-        var token = response.token;
-        console.log(token);
-      
-       // this.navigate('home');
-      
-      } else {
-          // error
-         error = res;
-         throw error;
-      }
-    } catch(error) {
-        console.log("error response: " + error);
-    }
-  }
-
-      storeToken(responseData){
-    AsyncStorage.setItem(ACCESS_TOKEN, responseData, (err)=> {
-      if(err){
-        console.log("Erros Existentes");
-        throw err;
-      }
-      console.log("Logou com successo");
-    
-    }).catch((err)=> {
-        console.log("O error é: " + err);
-    });
-  }
-
-  getToken(){
-      try {
-         var token = AsyncStorage.getItem(ACCESS_TOKEN);
-           if(!token) {
-                        console.log("Token inexistente");
-                        } else {
-                  this.verifyToken(token)
-              }
-
-                  console.log("Ver token" + token);
-    
-    } catch(error) {  
-
-    console.log("Erro");
-  }
-
-}
-
-  removeToken(){
-    try {
-      AsyncStorage.removeItem(ACCESS_TOKEN);
-      this.getToken();
-    }catch(error) {
-      console.log("erro");
-    }
-  }
-
-  login = () => {
-    try {
-   // alert(this.state.username);
-  fetch('https://suap.ifrn.edu.br/api/v2/autenticacao/token/', {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
+const Tab = TabNavigator (
+  {
+    Login: {
+      screen: Login,
+    },
+    Home: {
+      screen: Home,
+    },
+    Mapa: {
+      screen: Mapa,
+    },
   },
-            body: JSON.stringify({
-              username: this.state.username,
-              password: this.state.password
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+const Menu = DrawerNavigator(
+    {
+        Login: { screen: Login },
+        Home: { screen: Home },
+        Mapa: { screen: Mapa },
+    },
+    {
+        initialRouteName: 'Login',
+    }
+);
+
+const MenuButton = StackNavigator({
+  DrawerStack: { screen: Menu }
+}, {
+  headerMode: 'float',
+  navigationOptions: ({navigation}) => ({
+    title: 'SmartIF',
+    headerLeft: <Text onPress={() => navigation.navigate('DrawerOpen')}>Menu</Text>
   })
 })
-    .then((response) => response.json())
-    .then((response) => {
-      //let res = await response.Text();
-      if(response.token) {
-        //sucesso
-        var token = response.token;
-        console.log(token);
-        this.setState({error: ""});
-        // Em caso de sucesso, vamos armazenar o access_token no AsyncStorage
-        alert("token " + token);
-        this.storeToken(token);
-        //ERA PRA REDIRECIONAR PARA PASTA HOME - VER COM GUSTAVO
-       // this.redirect('home');  
-      } else {
-        //caso de erro
-        alert("Não foi possível autenticar!");
-      }
-    });
- 
-    } catch (error) {
-      this.removeToken();
-      this.setState({error: error});
-      console.log("error " + error);
+
+
+class App extends Component {
+        
+    constructor(props) {
+        super(props);
     }
-  }
+    
+    render() {
+        return <MenuButton />;
+    }
+   
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-   backgroundImage: {
-    flex: 1,
-    alignSelf: 'stretch',
-    width: null,
-    justifyContent: 'center',
-  },
-   content: {
-    alignItems: 'center',
-  },
-   logo: {
-    color: 'white',
-    fontSize: 40,
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    textShadowColor: '#252525',
-    textShadowOffset: {width: 2, height: 2},
-    textShadowRadius: 15,
-    marginBottom: 20,
-  },
-   inputContainer: {
-    margin: 20,
-    marginBottom: 0,
-    padding: 20,
-    paddingBottom: 10,
-    alignSelf: 'stretch',
-    borderWidth: 1,
-    borderColor: '#fff',
-    backgroundColor: 'rgba(255,255,255,0.2)',
-  },
-  input: {
-    textAlign: 'center',
-    fontSize: 16,
-    height: 40,
-    padding: 10,
-    marginBottom: 10,
-    backgroundColor: 'rgba(255,255,255,0.6)',
 
-  },
-   buttonContainer: {
-    alignSelf: 'stretch',
-    margin: 20,
-    padding: 20,
-    backgroundColor: 'blue',
-    borderWidth: 1,
-    borderColor: '#fff',
-    backgroundColor: 'rgba(255,255,255,0.6)',
-  },
-  buttonText: {
-    fontSize: 16,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-
-});
+export default App;
---
+++ mapa/AppMapa.js
@@ -0,0 +1,151 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity, 
+  Button,
+  Alert
+  } from 'react-native';
+import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
+import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps';
+//import MyLocationMapMarker from './mapa/MyLocationMapMarker';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = -6.261601;
+const LONGITUDE = -36.512239;
+const LATITUDE_DELTA = 0.0050;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+class AppMapa extends Component {
+        
+    constructor(props) {
+        super(props);
+        this.state = {
+            infoPosicaoAl: null,
+            latitudeAl: null,
+            longitudeAl: null,
+            erro: null,
+            region: {
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            },
+            coordinate: {
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+            },
+            latitudeAlc: LATITUDE,
+            longitudeAlc: LONGITUDE,
+            amount: 0,
+            enableHack: false,
+          };
+    }
+    
+    componentDidMount() {
+        this.watchId = navigator.geolocation.watchPosition(
+                (position) => {
+            this.setState({
+                latitudeAl: position.coords.latitude,
+                longitudeAl: position.coords.longitude,
+                latitudeAlc: position.coords.latitude, 
+                longitudeAlc: position.coords.longitude,
+                error: null,
+            });
+        },
+                (error) => this.setState({error: error.message, latitudeAlc: LATITUDE, longitudeAlc: LONGITUDE}),
+                {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10},
+                );
+        var la = this.state.latitudeAl;
+        var l = this.state.region.latitude;
+        var loa = this.state.longitudeAl;
+        var lo = this.state.region.longitude;
+        if ((la > l - 0.004) && (la < l + 0.004) && (loa > lo - 0.004) && (loa < lo + 0.004)) {
+            this.setState({infoPosicaoAl: 'Você está no IFRN de Currais Novos'});
+        } else {
+            this.setState({infoPosicaoAl: 'Você não está no IFRN de Currais Novos'});
+        }
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId);
+    }
+    
+    onRegionChange(region) {
+      this.setState({ region });
+    }
+    
+    render() {
+        return (
+                <View style={styles.container}>
+                    <Text>
+                    Latitude(PB):{this.state.region.latitude}
+                    </Text>
+                    <Text>
+                    Longitude(PB):{this.state.region.longitude}
+                    </Text>
+                    <Text>
+                    Latitude(Aluno):{this.state.latitudeAl}
+                    </Text>
+                    <Text>
+                    Longitude(Aluno):{this.state.longitudeAl}
+                    </Text>
+                    <Text>
+                    {this.state.infoPosicaoAl}
+                    </Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                    <MapView
+                      provider={this.props.provider}
+                      style={styles.map}
+                      initialRegion={this.state.region}
+                    >
+                      <MapView.Marker 
+                        coordinate={{latitude: this.state.latitudeAlc, longitude: this.state.longitudeAlc}}
+                        title={'Aluno'}
+                      />
+                      <MapView.Marker 
+                        coordinate={this.state.coordinate}
+                        title={'IFRN'}
+                        description={'IFRN'}
+                        image={require('./ifrnicon.png')}
+                      />
+                    </MapView>
+
+
+                  </View>
+	);
+    }
+   
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '70%'
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+
+  
+  });
+
+export default AppMapa;
