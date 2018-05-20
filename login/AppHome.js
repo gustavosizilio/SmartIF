bce983168ec4d3b982e9a74f0582e39c5e8d6e08
kairiroberto <kairiroberto@gmail.com>
2018-05-19 22:36:35 -0300

    Add files via upload

--- login/AppHome.js
+++ login/AppHome.js
@@ -1,104 +1,35 @@
-import React from 'react';
-import { Component, StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, AsyncStorage, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Button, TouchableOpacity, AsyncStorage, Alert, Dimensions } from 'react-native';
 import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
 
 const ACCESS_TOKEN = 'access_token';
 
-//var STORAGE_KEY = ACCESS_TOKEN;
-
-export default class Home extends React.Component {
-  constructor(props){
+export default class Home extends Component {
+  
+constructor(props){
     super(props);
-
     this.state = {
       accessToken: "",
-
+      turma: "",
     }
-
     this.getTurmas();
-  }
-
-   getToken(){
-      try {
-         accessToken = AsyncStorage.getItem(ACCESS_TOKEN);
-           if(!accessToken) {
-                        this.props.navigation.navigate('Login');
-                        } else {
-                  this.setState({accessToken: accessToken})
-                 // this.verifyToken(accessToken)
-              }
-
-                 // console.log("Ver token" + accessToken);
-    
+}
+  
+getToken(){
+    try {
+        accessToken = AsyncStorage.getItem(ACCESS_TOKEN);
+            if(!accessToken) {
+            } else {
+                this.setState({accessToken: accessToken})
+            }
     } catch(error) {  
-
-    console.log("Erro");
-  }
-
+    }
 }
 
- render() {
-
-    return(
-      <View style={styles.container}>
-        <Text style={styles.title}> Bem Vindo </Text>
-
-         
-        <TouchableOpacity onPress={this.onLogout.bind(this)} style={styles.button}>
-          <Text style={styles.buttonText}>
-            Logout
-          </Text>
-        </TouchableOpacity>
-       </View>
-    );
-  }
-
-  deleteToken() {
-    try {
-        AsyncStorage.removeItem(ACCESS_TOKEN);
-        console.log("Token removido");
-        this.props.navigation.navigate('Login');
-    } catch(error) {
-        console.log("Erros...");
-    }
-  }
-
-  onLogout(){
-    
-     this.deleteToken();
-  
-    }
-  
-  //Se o token for verificado, redirecionaremos o usuário para O HOME
-  verifyToken(token) {
-
-   var accessToken = token
-
-    try {
-     response = fetch('https://suap.ifrn.edu.br/api/v2/autenticacao/token/verify/'+accessToken);
-        if (response.token) {
-      //Token confirmado significa que o usuário está logado, então o redirecionamos para home
-        var verToken = response.token;
-        console.log(verToken);
-        //accessToken: this.state.accessToken
-        this.props.navigation.navigate('Login');
-      
-      } else {
-          // error
-         error = res;
-         throw error;
-      }
-    } catch(error) {
-        console.log("error response: " + error);
-    }
-  }
-
-  getTurmas() {
+getTurmas() {
     //var token = this.getToken();
-   AsyncStorage.getItem(ACCESS_TOKEN).then((token) => {
-
-      console.log("XXXXXX"+JSON.stringify(token));
-      fetch('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/turmas-virtuais/2018/1/', {
+    AsyncStorage.getItem(ACCESS_TOKEN).then((token) => {
+    fetch('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/turmas-virtuais/2018/1/', {
           method: 'GET',
           headers: {
               'Accept': 'application/json',
@@ -109,12 +40,49 @@ export default class Home extends React.Component {
       })
       .then((response) => response.json())
       .then((response) => {
-        alert(JSON.stringify(response));
-        console.log(JSON.stringify(response));  
+           this.setState(
+           {
+               turma: JSON.stringify(response)
+           }
+           );
       });
 
     });
-   }
+}
+
+  
+render() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}> Bem Vindo: </Text>
+            <Text style={styles.title}> {this.state.turma} </Text>
+        </View>
+    );
+}
+
+onLogout() {
+    try {
+        AsyncStorage.removeItem(ACCESS_TOKEN);
+        this.props.navigation.navigate('Login');
+    } catch(error) {    
+    }
+}
+  
+verifyToken(token) {
+    var accessToken = token
+    try {
+        response = fetch('https://suap.ifrn.edu.br/api/v2/autenticacao/token/verify/'+accessToken);
+        if (response.token) {
+            var verToken = response.token;
+            //this.props.navigation.navigate('Login');
+        } else {
+            error = res;
+            throw error;
+        }
+    } catch(error) {    
+    }
+}
+
 }
 
 const styles = StyleSheet.create({
---
+++ login/AppLogout.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Button, TouchableOpacity, AsyncStorage, Alert, Dimensions } from 'react-native';
+import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
+
+const ACCESS_TOKEN = 'access_token';
+
+
+class Logout extends Component {
+        
+    constructor(props) {
+        super(props);
+        try {
+            AsyncStorage.removeItem(ACCESS_TOKEN);
+        } catch(error) {    
+        }
+        props.navigation.navigate('App');
+    }
+    
+    render() {
+        return null;
+    }
+   
+}
+
+export default Logout;
+
